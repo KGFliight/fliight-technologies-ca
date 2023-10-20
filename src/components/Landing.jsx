48ef9080f97f05a312b7d08d35f1a0b314a62c56
James Boland <102449868+JRBoland@users.noreply.github.com>
2023-10-20 16:38:36 +1100

    homepage responsiveness, padding and margins, client logos

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,10 +15,10 @@ function Landing() {
       </span>
       </p>
       <br/>
-      <span className="vertical-align p-8 ml-4 lg:p-0 w-80 sm:ml-32 md:ml-32 lg:ml-40 mt-[25rem] sm:mt-[40rem] lg:mt-[40rem] sm:w-[28rem] lg:h-20 text-sm sm:text-md md:text-lg font-rajdhani font-thin leading-20 tracking-wide  text-ft-white">
+      <span className="vertical-align py-8 px-16 lg:p-0 w-full sm:ml-16 lg:ml-40 mt-[25rem] sm:mt-[36rem] md:mt-[36rem] lg:mt-[40rem] sm:w-[28rem] text-md sm:text-md md:text-lg font-rajdhani font-thin leading-20 tracking-wide  text-ft-white">
       We are a Wollongong based engineering firm at the forefront of design, manufacture and modification of custom Unmanned Aerial Vehicles (UAV's)
       </span>
-      <div className="bg-deltaquad h-1/4 sm:h-1/3 lg:h-2/3 xl:h-screen lg:max-h-full max-w-[780px] md:max-w-[1366px] lg:max-w-full bg-cover w-screen flex justify-start items-start mt-56 md:mt-56 lg:mt-0 ml-auto">
+      <div className="bg-deltaquad h-1/3 sm:h-1/3 lg:h-2/3 xl:h-screen lg:max-h-full max-w-[780px] md:max-w-[1366px] lg:max-w-full bg-cover w-screen flex justify-start items-start mt-44 md:mt-56 lg:mt-0 ml-auto">
       </div>
       
       <button className="fixed-bottom w-full md:hidden bg-rose-700 h-10 px-5 py-2 inline-flex justify-center items-center">
@@ -26,7 +26,7 @@ function Landing() {
       </button>
       </div>
       <div className="flex flex-col items-center justify-center my-40 sm:my-80">
-        <p className="w-80 h-24 text-center text-2xl font-regular leading-loose tracking-wide">We serve clients worldwide, providing specialised solutions to unique problems.</p>
+        <p className="w-80 sm:w-1/2 lg:w-1/3 h-24 text-center text-2xl font-regular leading-loose tracking-wide">We serve clients worldwide, providing specialised solutions to unique problems.</p>
       </div>
       <LandingIcons/>
     </div>
--- src/components/LandingIcons.jsx
+++ src/components/LandingIcons.jsx
@@ -1,13 +1,29 @@
 import '../App.css'
+import deltaquad from '../assets/images/icons/deltaquad.png'
+import dji from '../assets/images/icons/dji.png'
+import hover from '../assets/images/icons/hover.png'
+import unleash from '../assets/images/icons/unleash.png'
+import pypervision from '../assets/images/icons/pypervision.png'
 
 function LandingIcons() {
   return (
-    <div className="flex flex-col justify-center items-center h-40 my-32">
-      <ul className="flex flex-col gap-12">
-        <li className="border w-60 h-12 items-center justify-center flex">Icon1</li>
-        <li className="border w-60 h-12 items-center justify-center flex">Icon2</li>
-        <li className="border w-60 h-12 items-center justify-center flex">Icon3</li>
-        <li className="border w-60 h-12 items-center justify-center flex">Icon4</li>
+    <div className="flex flex-col justify-center items-center my-32 grayscale">
+      <ul className="flex flex-col gap-16">
+        <li className="w-60 h-20 items-center justify-center flex">
+          <img src={deltaquad} alt="deltaquad logo" />
+        </li>
+        <li className="w-60 h-20 items-center justify-center flex">
+          <img src={dji} alt="dji logo" />
+        </li>
+        <li className="w-60 h-20 items-center justify-center flex">
+          <img src={hover} alt="hover logo" />
+        </li>
+        <li className="w-60 h-20 items-center justify-center flex">
+          <img src={unleash} alt="pypervision logo" />
+        </li>
+        <li className="w-60 h-20 items-center justify-center flex">
+          <img src={pypervision} alt="pypervision logo" />
+        </li>
       </ul>
     </div>
   )
