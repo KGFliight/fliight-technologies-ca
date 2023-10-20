854c7a4b254e3573ce43c4116b621486fe31fb6d
James Boland <102449868+JRBoland@users.noreply.github.com>
2023-10-20 14:58:15 +1100

    landing page responsiveness

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,22 +3,20 @@ import '../App.css'
 function Landing() {
   return (
     <div className="w-screen h-screen flex-col justify-start items-start inline-flex gap-7">
-      <p className="vertical-align p-8 lg:p-0 lg:ml-48 w-fit h-fit text-5xl md:text-7xl font-rajdhani font-regular uppercase leading-20 tracking-widest text-ft-white items-center">
+      <p className="vertical-align p-4 mt-4 md:mt-24 ml-8 sm:ml-16 md:ml-24 lg:ml-40 w-3/4 overflow-visible lg:w-1/3 h-fit text-4xl sm:text-7xl font-rajdhani font-regular uppercase leading-20 tracking-widest text-ft-white items-center">
       UAV SOLUTIONS
-      <br/>
       CUSTOMISED TO
-      <br/>
       WORK FOR YOU
       <br/>
-      <span className="ml-4 h-60 text-2xl font-rajdhani font-regular uppercase leading-20 tracking-widest text-ft-white">
+      <span className="ml-4 h-60 text-sm md:text-lg lg:text-2xl font-rajdhani font-regular uppercase leading-20 tracking-widest text-ft-white">
       > WATCH VIDEO
       </span>
       </p>
       <br/>
-      <span className="vertical-align p-8 lg:p-0 w-80 lg:ml-48 mt-[32rem] md:mt-[32rem] 2xl:mt-[38rem] sm:w-[28rem] lg:h-20 text-lg font-rajdhani font-thin leading-20 tracking-wide text-ft-white">
+      <span className="vertical-align p-8 ml-4 lg:p-0 w-80 sm:ml-32 md:ml-32 lg:ml-40 mt-[28rem] sm:mt-[40rem] lg:mt-[40rem] sm:w-[28rem] lg:h-20 text-sm sm:text-md md:text-lg font-rajdhani font-thin leading-20 tracking-wide  text-ft-white">
       We are a Wollongong based engineering firm at the forefront of design, manufacture and modification of custom Unmanned Aerial Vehicles (UAV's)
       </span>
-      <div className="bg-deltaquad h-[32rem] md:h-screen 2xl:max-w-[2428px] lg:max-w-[1920px] sm:max-w-[960px] lg:min-w-[512px] sm:min-w-[512px] bg-cover p-24 w-screen flex justify-start items-start mt-44 md:mt-12 lg:ml-60">
+      <div className="bg-deltaquad h-1/4 sm:h-1/3 lg:h-2/3 xl:h-screen lg:max-h-full max-w-[780px] md:max-w-[1366px] lg:max-w-full bg-cover w-screen flex justify-start items-start mt-56 md:mt-56 lg:mt-0 ml-auto">
       </div>
     </div>
   )
