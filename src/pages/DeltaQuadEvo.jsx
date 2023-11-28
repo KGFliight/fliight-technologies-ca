04ed9d01c669247aec0c6d844f4df6c8127f9a0e
James Boland <102449868+JRBoland@users.noreply.github.com>
2023-11-28 15:22:34 +1100

    deltaquad evo page layout

--- src/pages/DeltaQuadEvo.jsx
+++ src/pages/DeltaQuadEvo.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import DeltaQuadBanner from '../components/DeltaQuadXxxBanner'
+import deltaQuadEvoBannerImage from '../assets/images/deltaquad-evo-banner.jpg'
+import DeltaQuadProInfo from '../components/DeltaQuadProInfo'
+import DeltaQuadVtol from '../components/DeltaQuadVtol'
 
 function DeltaQuadEvo() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 flex items-center justify-center">
-      <h1 className="text-6xl md:text-7xl font-medium leading md:leading-10 tracking-widest mt-24 py-20 justify-center flex uppercase h-20 my-40">DeltaQuad Evo</h1>
-      </main>
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <DeltaQuadBanner title="DeltaQuad Evo" imageSrc={deltaQuadEvoBannerImage} />
+      </div>
+      <DeltaQuadProInfo />
+      <div className="flex flex-col items-center my-20">
+        <div className="h-auto max-w-[1200px] z-50">
+        </div>
+      </div>
+      <DeltaQuadVtol />
       <Footer />
     </div>
   )
--- src/pages/DeltaQuadPro.jsx
+++ src/pages/DeltaQuadPro.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import DeltaQuadProBanner from '../components/DeltaQuadProBanner'
+import DeltaQuadBanner from '../components/DeltaQuadXxxBanner'
 import DeltaQuadProInfo from '../components/DeltaQuadProInfo'
 import deltaQuadProImage from '../assets/images/deltaquad-pro.jpeg'
 import DeltaQuadVtol from '../components/DeltaQuadVtol'
 import '../App.css'
+import deltaQuadProBannerImage from '../assets/images/deltaquad-pro-banner.jpg'
 
 function DeltaQuadPro() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex-1 flex flex-col items-center justify-center">
-        <DeltaQuadProBanner />
+        <DeltaQuadBanner
+          title="DeltaQuad Pro"
+          imageSrc={deltaQuadProBannerImage}
+        />
       </div>
       <DeltaQuadProInfo />
       <div className="flex flex-col items-center my-20">
-      <div className="h-auto max-w-[1200px] z-50">
-        <div className="image-container mx-8 md:mx-24 ">
-          <img src={deltaQuadProImage} className="object-scale-down " />
+        <div className="h-auto max-w-[1200px] z-50">
+          <div className="image-container mx-8 md:mx-24 ">
+            <img src={deltaQuadProImage} className="object-scale-down " />
+          </div>
         </div>
       </div>
-      
-      </div>
-      <DeltaQuadVtol/>
+      <DeltaQuadVtol />
       <Footer />
     </div>
   )
